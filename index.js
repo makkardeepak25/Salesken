f2f7dd564fc1618b976fb50986a5b3d0739a98a6
makkardeepak25 <makkardeepak25@gmail.com>
2021-09-04 22:21:06 +0530

    writing canvas for bar chat

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-var k="deepak"
+var n = 4;
+var arr = new Array(n * 60).fill(0);
+for (var i = 0; i < arr.length; i++) {
+  var num = Math.floor(Math.random() * 100);
+  arr[i] = num;
+}
+
+window.onload = function() {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  var width = 8;
+  var currX = 30;
+  var base = 300;
+  ctx.fillStyle = "lightgrey";
+  for (var i = 0; i < arr.length; i++) {
+    var h = arr[i];
+    ctx.fillRect(currX, base - h, width, h);
+    currX += width + 5;
+  }
+};
+
+var btn1 = document.getElementById("btn1");
+
+btn1.addEventListener("click", play);
+
+function play() {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+//   var n = 4;
+//   var arr = new Array(n * 60).fill(0);
+//   for (var i = 0; i < arr.length; i++) {
+//     var num = Math.floor(Math.random() * 100);
+//     arr[i] = num;
+//   }
+  var width = 8;
+  var currX = 30;
+  var base = 300;
+    for (let i = 0; i < arr.length; i++) {
+        var h = arr[i];
+    var st = setTimeout(function() {
+        ctx.fillRect(currX, base - h, width, h);
+        ctx.fillStyle = "green";
+      currX += width + 5;
+    }, 1000 * i);
+  }
+}
